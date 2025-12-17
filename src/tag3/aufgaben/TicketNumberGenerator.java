612f562a3bd63e821413211ac00872562e98be08
Administrator PC Damago GmbH <info@damago.eu>
2025-12-17 13:05:14 +0100

    Add TicketNumberGenerator class for unique ticket number generation

---
+++ src/tag3/aufgaben/TicketNumberGenerator.java
@@ -0,0 +1,42 @@
+package tag3.aufgaben;
+
+
+class SupportService {
+
+}
+
+class OderService {
+
+}
+
+
+public class TicketNumberGenerator {
+
+
+  private static TicketNumberGenerator instance;
+  private int ticketNumber;
+  private boolean initialized = false;
+
+  private TicketNumberGenerator() {
+  }
+
+  public static synchronized TicketNumberGenerator getInstance() {
+    if (instance == null) {
+      instance = new TicketNumberGenerator();
+    }
+    return instance;
+  }
+
+  public synchronized void setStart(int start) {
+    if (!initialized) {
+      ticketNumber = start;
+      initialized = true;
+    } else {
+      throw new IllegalStateException("TicketNumberGenerator wurde bereits initialisiert");
+    }
+  }
+
+  public synchronized int nextTicketNumber() {
+    return ticketNumber++;
+  }
+}
